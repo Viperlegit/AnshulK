ef8d72a8a9c6387beca3eb12991968e9f813effa
Viperlegit <ultralegend.og@gmail.com>
2026-09-24 00:00:41 +0530

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,14 @@
+num1 = 10
+operator = "/"
+num2 = 5
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 / num2)
+else:
+    print("invalid opreator")
