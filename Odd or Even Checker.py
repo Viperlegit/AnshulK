9ce648c5cfe5806a60b54897aacb071918c6c66b
Viperlegit <ultralegend.og@gmail.com>
2026-09-24 00:06:00 +0530

    Add files via upload

---
+++ Odd or Even Checker.py
@@ -0,0 +1,6 @@
+num = 9
+
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
---
+++ Temperature Converter.py
@@ -0,0 +1,7 @@
+c = 25
+
+f = (c * 9/5) + 32
+k = c + 273.15
+
+print("Fahrenheit =", f)
+print("Kelvin =", k)
